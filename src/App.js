3a09c68853c0982f10d6bb6e5a2d953ba239ba9a
ajmris <szymektarasiewicz@onet.pl>
2024-05-09 18:21:56 +0200

    cleaning input

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ class ToDoList extends Component{
     const {tasks, draft}=this.state
     const list=tasks
     list.push(draft)
-    this.setState({tasks: list})
+    this.setState({tasks: list, draft: ''})
   }
   render(){
+    const {title}=this.props
+    const {tasks, draft}=this.state
     return(
       <div>
-        <h1>{this.props.title}</h1>
-        {this.props.tasks.map(task=> <div><p>{task}</p></div>)}
-        <input type='text' onChange={this.updateDraft} />
+        <h1>{title}</h1>
+        {tasks.map(task=> <div><p>{task}</p></div>)}
+        <input type='text' onChange={this.updateDraft} value={draft}/>
         <button onClick={this.addToDo}>Add</button>
       </div>
     )
