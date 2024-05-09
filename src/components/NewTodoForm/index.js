14819bac0c34a4e607a3830f02b10f08adb55325
ajmris <szymektarasiewicz@onet.pl>
2024-05-09 19:51:24 +0200

    Restructuring code - components and containers.

---
+++ src/components/NewTodoForm/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+const NewTodoForm=(props)=>(
+    <div>
+      <input type='text' onChange={props.onChange} value={props.draft}/>
+        <button onClick={props.onSubmit}>Add</button>
+    </div>
+  )
+export default NewTodoForm
