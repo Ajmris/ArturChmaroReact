040c433fd0362cdb24bcab6ca3e25538134075e9
ajmris <szymektarasiewicz@onet.pl>
2024-05-16 17:32:35 +0200

    Styled component

--- src/components/NewTodoForm/index.js
+++ src/components/NewTodoForm/index.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Button=styled.button`
+  background: #db6a00;
+  color: #00a7fa;
+  width: 80px;
+  height: 32px;
+  font-size: 1.7em;
+  border: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;`
+const TextInput=styled.input`
+  padding:5px;
+  font-size: 1.2em;
+  background: #232632;
+  color: #d3d4d6;
+  width: 100%;
+  margin-right: 7px;
+  border: 0px;
+  -webkit-appearance: none;`
+const Container=styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid #343744;
+  background: #232632;
+  border-radius: 10px;
+  padding: 5px;
+`
 const NewTodoForm=(props)=>(
-    <div>
-      <input type='text' onChange={props.onChange} value={props.draft}/>
-        <button onClick={props.onSubmit}>Add</button>
-    </div>
+    <Container>
+      <TextInput type='text' onChange={props.onChange} value={props.draft}/>
+      <Button onClick={props.onSubmit}>+</Button>
+    </Container>
   )
 export default NewTodoForm
